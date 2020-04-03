0885117ec7c5adc7a0e46bc7ba2d65f4083bdc0a
Teatoller <steven.otieno1@gmail.com>
2020-04-03 10:48:05 +0300

    (image-upload): Implement image upload
    - add function to handle image upload
    
    [Starts #004]

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -11,4 +11,18 @@ class ImageUploadController extends Controller
    {
        return view('home');
    }
+
+   public function uploadImages(Request $request)
+   {
+       $this->validate($request,[
+           'image_name'=>'required|mimes:jpeg,bmp,jpg,png|between:1, 6000',
+       ]);
+
+       $image_name = $request->file('image_name')->getRealPath();;
+
+       Cloudder::upload($image_name, null);
+
+       return redirect()->back()->with('status', 'Image Uploaded Successfully');
+
+   }
 }
