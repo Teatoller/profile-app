1f1a22494eac2d1b30bcc88824a75f0d90694ebb
Teatoller <steven.otieno1@gmail.com>
2020-04-03 11:31:28 +0300

    (schema): Implement schema design
    - add image in schema
    - add image url in chema design
    - modify app provider
    
    [Starts #008]

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -2,27 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use JD\Cloudder\Facades\Cloudder;
 use Illuminate\Http\Request;
+use JD\Cloudder\Facades\Cloudder;
 
 class ImageUploadController extends Controller
 {
     public function home()
-   {
-       return view('home');
-   }
+    {
+        return view('home');
+    }
 
-   public function uploadImages(Request $request)
-   {
-       $this->validate($request,[
-           'image_name'=>'required|mimes:jpeg,bmp,jpg,png|between:1, 6000',
-       ]);
+    public function uploadImages(Request $request)
+    {
+        $this->validate($request, [
+            'image_name' => 'required|mimes:jpeg,bmp,jpg,png|between:1, 6000',
+        ]);
 
-       $image_name = $request->file('image_name')->getRealPath();;
+        $image = $request->file('image_name');
 
-       Cloudder::upload($image_name, null);
+        $name = $request->file('image_name')->getClientOriginalName();
 
-       return redirect()->back()->with('status', 'Image Uploaded Successfully');
+        $image_name = $request->file('image_name')->getRealPath();
 
-   }
+        Cloudder::upload($image_name, null);
+
+        list($width, $height) = getimagesize($image_name);
+
+        $image_url = Cloudder::show(Cloudder::getPublicId(), ["width" => $width, "height" => $height]);
+
+        //save to uploads directory
+        $image->move(public_path("uploads"), $name);
+
+        //Save images
+        $this->saveImages($request, $image_url);
+
+        return redirect()->back()->with('status', 'Image Uploaded Successfully');
+
+    }
 }
